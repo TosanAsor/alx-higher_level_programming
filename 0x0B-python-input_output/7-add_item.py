6398bdef6e544b0ba76f678f78dcd4e8b15bdf0e
louisa asor <louisa.asor@gmail.com>
2023-01-10 07:35:48 -0800

    python input

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module adds all arguments to a Python list and save them to a file."""
+
+
+import sys
+
+if _name_ == "_main_":
+    save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        _import_('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
